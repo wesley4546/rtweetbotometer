5b43207384957c843312bb3c900e268d0254c34c
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-02-18 13:58:24 -0500

    Update rtweetstream.R

--- rtweetstream.R
+++ rtweetstream.R
@@ -2,15 +2,24 @@
 library(rtweet)
 
 
-# ,#WhiteLivesMatter,#WhiteSupremacy,
+# #WhiteLivesMatter,#WhiteSupremacy,
 # #WhiteSupremacist,#whitepower,#WhitePride,#WhiteNatalism,#WhitePro,
 # #mywhiteprivilege,#populationreplacement,#chrislam,#clownworld,#honkhonk")
 
+#Hashtags
+hastags <- c("#trump")
 
-hastags <- c("starbucks,coffee,trump")
+#Streamtime
 streamtime <- 30
+
+#Name of file
 filename <- "rtweet.json"
 
-## Stream 
+## Stream initalizing
 rt <- stream_tweets(q = hastags, timeout = streamtime, file_name = filename)
+
+# Creates dataframe
+rt <- parse_stream("rtweet.json")
+
+
 
