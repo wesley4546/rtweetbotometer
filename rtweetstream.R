bdb00506a11b807f26982096699ebeb412e16245
Dre <ergosumdre@gmail.com>
2020-02-18 14:52:39 -0500

    Update rtweetstream.R

--- rtweetstream.R
+++ rtweetstream.R
@@ -1,16 +1,25 @@
-#install.package("rtweet")
+install.packages("rtweet")
 library(rtweet)
 
 
-# ,#WhiteLivesMatter,#WhiteSupremacy,
-# #WhiteSupremacist,#whitepower,#WhitePride,#WhiteNatalism,#WhitePro,
-# #mywhiteprivilege,#populationreplacement,#chrislam,#clownworld,#honkhonk")
+hashtags <-  ("#WhiteLivesMatter,#WhiteSupremacy,
+#WhiteSupremacist,#whitepower,#WhitePride,#WhiteNatalism,#WhitePro,
+#mywhiteprivilege,#populationreplacement,#chrislam,#clownworld,#honkhonk")
 
+??rtweet
 
-hastags <- c("starbucks,coffee,trump")
-streamtime <- 30
+remove(access_secret)
+remove(access_token)
+remove(consumer_key)
+remove(consumer_secret)
+
+get_tokens()
+
+hastags <- c("#WhiteLivesMatter,#WhiteSupremacy,#WhiteSupremacist,#whitepower, #WhitePride,#WhiteNatalism,#WhitePro,#mywhiteprivilege,#populationreplacement, #chrislam,#clownworld,#honkhonk")
+streamtime <- 172800
 filename <- "rtweet.json"
 
 ## Stream 
 rt <- stream_tweets(q = hastags, timeout = streamtime, file_name = filename)
+
 
