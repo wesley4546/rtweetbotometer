20ef93343671144506d30ec3b63c8ff0eaf90a19
Wesley G <56905659+wesley4546@users.noreply.github.com>
2020-02-23 20:31:43 -0500

    Started before debate collection

---
+++ 2WeekCollection/rtweetcollection2week.R
@@ -0,0 +1,29 @@
+#install.packages("rtweet")
+library(rtweet)
+library(dplyr)
+
+## Collecting
+
+#Parameters
+ hashtags <- c("#antiwhite,#WhiteSupremacy,#WhitePride,#mywhiteprivilege,#clownworld")
+ streamtime <- 172800
+ filename <- "rtweetbeforedebate.json"
+
+# Stream initalizing
+ rt <- stream_tweets(q = hashtags, timeout = streamtime, file_name = filename, language="en")
+
+ 
+ 
+## Cleaning
+ 
+# Creates dataframe
+rt <- parse_stream("rtweet7day.json")
+
+
+
+clean_rt <- rt %>%
+  filter(is_retweet == FALSE, !is.na(hashtags), is_quote == FALSE) %>% 
+  select(user_id,screen_name,hashtags,text)
+names <- unique(unlist(clean_rt$screen_name))
+
+
